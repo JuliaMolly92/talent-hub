e185caa865e54bb3cee785b08f622d164bdd05d2
Julia Molly Anwar <juliamolly92@gmail.com>
2021-11-25 22:39:22 +0000

    added slider looping functionality and responsivness to lower bar

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 // VARIABLES
 const hamburger = document.querySelector('.hamburger');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const arrowPreview = document.querySelector('.slider-button-prev');
+const arrowPrev = document.querySelector('.slider-button-prev');
 const arrowNext = document.querySelector('.slider-button-next');
 const totalSliders = document.querySelectorAll('.slider');
 const sliderBar = document.querySelector('.slider-bar-selector');
+let slideBarSelectorWidth = document.querySelector('.slider-bar-selector').offsetWidth;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 // !!!!!!! THIS NEEDS TO BE UPDATED ON EVERY SCREEN ADJUSTMENT !!!!!!!
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-const slideBarSelectorWidth = window.innerWidth / 7;
+window.addEventListener('resize', function() {
+    slideBarSelectorWidth = document.querySelector('.slider-bar-selector').offsetWidth;
+    sliderBar.style.left = slideBarSelectorWidth * parseInt(document.querySelector('.slider.active').getAttribute('data-slider')-1) + 'px';
+});
 
 // Adding and removing open/close classes for hamburger
 hamburger.addEventListener('click', function() {
@@ -28,24 +32,51 @@ hamburger.addEventListener('click', function() {
 });
 
 // Sliderers moving back and forth
+function removeaddClasses (activeSlider, num, firstClass, secondClass) {
+    totalSliders[parseInt(activeSlider.getAttribute('data-slider'))-1].classList.remove('active');
+    totalSliders[parseInt(activeSlider.getAttribute('data-slider'))-1].classList.add(firstClass);
+    totalSliders[num].classList.add('active');
+    totalSliders[num].classList.remove(secondClass);
+
+    sliderBar.style.left = slideBarSelectorWidth * num + 'px';
+}
+
+// LOOPING SLIDER
+function restartSlider(x, firstClass, secondClass) {
+    totalSliders.forEach(el => {
+        el.classList.remove('active', firstClass);
+        el.classList.add(secondClass);
+    });
+
+    totalSliders[x].classList.add('active');
+    totalSliders[x].classList.remove(secondClass);        
+};
+
+// PREVIOUS SLIDE
+arrowPrev.addEventListener('click', function() {
+    const activeSlider = document.querySelector('.slider.active');
+    const num = parseInt(activeSlider.getAttribute('data-slider'))-2;
+
+    if (num == -1) {
+        restartSlider(6, 'slider-next', 'slider-prev');
+        sliderBar.style.left = slideBarSelectorWidth * 6 + 'px';
+    }
+    else {
+        removeaddClasses(activeSlider, num, 'slider-next', 'slider-prev');
+    }
+});
+
+// NEXT SLIDE
 arrowNext.addEventListener('click', function() {
     const activeSlider = document.querySelector('.slider.active');
-    const barLeft = slideBarSelectorWidth * parseInt(activeSlider.getAttribute('data-slider'));
+    const num = parseInt(activeSlider.getAttribute('data-slider'));
 
-    if(parseInt(activeSlider.getAttribute('data-slider')) == totalSliders.length) {
-        totalSliders.forEach(el => {
-            el.classList.remove('active', 'slider-prev');
-            el.classList.add('slider-next');
-        });
-        totalSliders[0].classList.add('active');
-        totalSliders[0].classList.remove('slider-next');
+    if (parseInt(activeSlider.getAttribute('data-slider')) == totalSliders.length) {
+        restartSlider(0, 'slider-prev', 'slider-next');
         sliderBar.style.left = '0px';
     }
     else {
-        totalSliders[parseInt(activeSlider.getAttribute('data-slider'))-1].classList.remove('active');
-        totalSliders[parseInt(activeSlider.getAttribute('data-slider'))-1].classList.add('slider-prev');
-        totalSliders[parseInt(activeSlider.getAttribute('data-slider'))].classList.add('active');
-        totalSliders[parseInt(activeSlider.getAttribute('data-slider'))].classList.remove('slider-next');
-        sliderBar.style.left = barLeft + 'px';
+        removeaddClasses(activeSlider, num, 'slider-prev', 'slider-next');
     }
-})
+
+});
